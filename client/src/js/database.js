5720988c081400f81a45ad7cc4f0b379d97e55bf
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-17 18:57:39 -0400

    added method to grab content from Db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,9 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Class info - TODO: Add logic to a method that accepts some content and adds it to the database
+
+// Exports the putDB and accepts content in its database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -23,7 +25,20 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Class info - TODO: Add logic for a method that gets all the content from the database
+
+// Grabs the content from the database 
+export const getDb = async () => {
+  console.log('GET from the databases');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(0);
+  const result = await request;
+  if (typeof result === 'undefined') {
+    return null;
+  }
+  return result.jateContent;
+}
 
 initdb();
